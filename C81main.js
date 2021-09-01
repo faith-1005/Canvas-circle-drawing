7ec7565f762831fe6a850c15703f54f25b8f0e33
faith-1005 <84843388+faith-1005@users.noreply.github.com>
2021-09-01 17:50:32 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,43 @@
+canvas=document.getElementById("my_Canvas");
+ctx=canvas.getContext("2d");
+
+color="#6b705c";
+
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(200,200,40,0,2*Math.PI);
+ctx.stroke();
+
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e)
+{
+    color=document.getElementById("color").value;
+    console.log(color);
+
+    mouse_x=e.clientX-canvas.offsetLeft;
+    mouse_y=e.clientY-canvas.offsetTop;
+    
+    console.log("x = "+mouse_x+", y = "+mouse_y);
+    circle(mouse_x, mouse_y);
+
+}
+
+
+function circle(mouse_x, mouse_y)
+{
+    ctx.beginPath();
+    ctx.strokeStyle=color;
+    ctx.lineWidth=2;
+    ctx.arc(mouse_x,mouse_y,40,0,2*Math.PI);
+    ctx.stroke();
+
+}
+
+
+function cleararea()
+{
+    ctx.clearRect(0,0, canvas.width,canvas.height);
+}
